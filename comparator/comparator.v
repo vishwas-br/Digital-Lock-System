cad24891b957264e0d9d3cdf0c5537b142da1d16
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2026-02-14 17:57:09 +0530

    Create comparator.v

---
+++ comparator/comparator.v
@@ -0,0 +1,15 @@
+module comparator #(parameter CODE_LEN = 4)
+                   (input wire [CODE_LEN*4 - 1 : 0] entered_code,
+                    input wire [CODE_LEN*4 - 1 : 0] stored_code,
+                    output reg match
+                   );
+
+
+ 
+ always @(*) begin
+  if (entered_code == stored_code)
+    match = 1'b1;
+  else 
+    match = 1'b0;
+ end
+endmodule
