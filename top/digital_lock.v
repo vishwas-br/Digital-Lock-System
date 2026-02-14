56115493aeb1b6072c44d5a6a5d1e9a8505ccc18
Vishwas B R <62811671+vishwas-br@users.noreply.github.com>
2026-02-14 18:08:14 +0530

    Create digital_lock.v

---
+++ top/digital_lock.v
@@ -0,0 +1,51 @@
+module digital_lock #(
+    parameter CODE_LEN = 4
+)(
+    input  wire clk,
+    input  wire rst_n,
+    input  wire digit_valid,
+    input  wire submit,
+    input  wire [3:0] digit_in,
+    input  wire [CODE_LEN*4-1:0] stored_code,  
+
+    output wire unlock_led,
+    output wire fail_led,
+    output wire ready_for_input
+);
+
+    wire [CODE_LEN*4-1:0] entered_code;
+    wire full;
+    wire match;
+
+    // Shift Register Bloc
+    input_handle #(.CODE_LEN(CODE_LEN)) u_shift (
+        .clk(clk),
+        .rst_n(rst_n),
+        .digit_in(digit_in),
+        .digit_valid(digit_valid),
+        .entered_code(entered_code),
+        .full(full),
+        .ready(ready),
+        .submit(submit)
+    );
+
+    // Comparator Block
+    comparator #(.CODE_LEN(CODE_LEN)) u_comp (
+        .entered_code(entered_code),
+        .stored_code(stored_code),
+        .match(match)
+    );
+
+    // Control FSM Block
+    control u_fsm (
+        .clk(clk),
+        .rst_n(rst_n),
+        .full(full),
+        .submit(submit),
+        .match(match),
+        .unlock_led(unlock_led),
+        .fail_led(fail_led),
+        .ready_for_input(ready_for_input)
+    );
+
+endmodule
